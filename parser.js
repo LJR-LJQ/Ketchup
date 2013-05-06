9583845ee0a23947bfb348fc0b72ec683aee4c5e
Jianru Lin <jianru.lin@gmail.com>
2013-05-06 15:51:05 +0800

    初步完成了算法，但尚未测试

--- parser.js
+++ parser.js
@@ -8,123 +8,94 @@ exports.parse = parse;
 	2、callback：解析完毕后的回调函数
 	返回值：
 	如果解析失败返回 false（布尔类型）
-	如果解析成功返回一个对象，该对象形如： {name: '节点名称', children: [...子节点对象...]}
+	如果解析成功返回一个对象，该对象形如： {name: '节点名称', level: 数字, children: [...子节点对象...]}
 
 	函数行为说明：
 	本函数能够解析符合番茄酱(Ketchup)语言的字符串并构造出语义等价的AST语法树返回
 */
-function parse(inputBuf) {
+function parse(inputStr) {
+	// 流程概述
+	// 1、将输入字符串分割成行数组
+	// 2、解析每一行，将其转换为节点对象
+	// 3、分析节点对象间的缩进关系，将他们连接成树
+	// 4、返回树的根节点
+
 	// [变量]
 	var rootNode,
-		inputObj;
+		nodeList;
 
 	// [流程]
-	rootNode = {
-		name: 'root',
-		children: []
-	};
-	inputObj = {
-		str: inputBuf.toString('utf8', 0, inputBuf.length)
+	rootNode = node('root', -1);debugger;
+
+	if (typeof inputStr !== 'string' || inputStr.length < 1) {
+		// inputStr 不是字符串（例如 null、undefined、其他类型），或者是空字符串
+		// 则直接返回根节点即可
+		return rootNode;
 	}
-	traverse(rootNode, inputObj, -1);
-	console.log(rootNode.children[0].children);
-	return rootNode.children[0];
+
+	nodeList = toLines(inputStr).map(function(line) {
+		return parseLine(line);
+	});
+
+	nodeList.unshift(rootNode);
+
+	toTree(nodeList);
+
+	return rootNode;
 
 	// [函数]
-	function traverse(root, inputObj, fartherSpaceCount) {
-		// [变量]
-		var firstLineStr,
-			remainStr,
-			spaceCount,
-			node;
-
-		// [流程]
-		while(true) {
-			if(inputObj.str.length <= 0) break; // 结束
-
-			firstLineStr = getFirstLine(inputObj.str, function(outputStr){
-				remainStr = outputStr;
-			});
-
-			spaceCount = getSpaceCount(firstLineStr, function(outputStr){
-				firstLineStr = outputStr;
-			});
-
-			if(spaceCount > fartherSpaceCount) { // 缩进
-				node = parseLineToNode(firstLineStr);
-				inputObj.str = remainStr;
-				traverse(node, inputObj, spaceCount);
-				root.children.push(node);
-			} else {
-				break;
-			}
-
-		}
+	function node(name, level) {
+		return {name: name, level: undefined, children: []};
 	}
 
-	function getFirstLine(inputStr, callback) {
-		// [变量]
-		var outputStr,
-			length;
-
-		// [流程]
-		length = inputStr.length;
-		for(var i = 0; i < length; i++) {
-			if(inputStr.charAt(i) == '\n') break;
-		}
-		outputStr = inputStr.slice(i + 1, length);
-		if(callback != null) callback(outputStr);
-		return inputStr.slice(0, i + 1).replace(/[\r\n]/g, '');
+	function toLines(inputStr) {
+		return inputStr.split('\r\n');
 	}
 
-	function getSpaceCount(inputStr, callback) {
-		// [变量]
-		var outputStr,
-			length;
+	function parseLine(line) {
+		var name,
+			level;
 
-		// [流程]
-		length = inputStr.length;
-		for(var i = 0; i < length; i++) {
-			if(inputStr.charAt(i) != ' ') {
-				outputStr = inputStr.slice(i, length);
-				if(callback != null) callback(outputStr);
-				return i;
+		level = countLeadingSpace(line);
+		name = line.substring(level, line.length);
+		return node(name, level);
+
+		function countLeadingSpace(line) {
+			for (var i = 0, len = line.length; i < len; ++i) {
+				if (line[i] !== ' ') return i;
 			}
 		}
 	}
 
-	function parseLineToNode(inputStr) {
+	function toTree(nodeList) {
+		// 思路
+		// 对于节点列表(nodeList)中位置在 i 处的元素 e
+		// 它的父元素的位置一定小于 i
+		// 因此确定其父元素的关键是向比 i 小的方向查找
+		// 也就是倒着向上查找
+		// 当我们碰到第一个缩进级别(level)比 e 小的元素时
+		// 可以肯定它就是 e 的父元素
+
 		// [变量]
-		var node,
-			length;
+		var e,
+			parent;
 
 		// [流程]
-		node = {
-			name: '',
-			attribute: {},
-			text: '',
-			children: []
-		};
-
-		// 提取标签名字
-		length = inputStr.length;
-		for(var i = 0; i < inputStr.length; i++) {
-			var c = inputStr.charAt(i);
-			if(!(isAlpha(c) || isUnderline(c))) break;
+		while (nodeList.length > 0) {
+			e = nodeList.pop();
+			parent = findParent(e, nodeList);
+			parent.children.unshift(e);
 		}
-		node.name = inputStr.slice(0, i);
-		// 下面分析标签属性，比较复杂，暂时写到这
-		inputStr = inputStr.slice(i, length);
-		length = inputStr.length;
-		return node;
-	}
 
-	// [函数]
-	function isAlpha(cCheck) {
-		return ((('a'<=cCheck) && (cCheck<='z')) || (('A'<=cCheck) && (cCheck<='Z')));
-	}
+		return e;
 
-	function isUnderline(cCheck) {
-		return ('_' == cCheck);
+		// [函数]
+		function findParent(e, nodeList) {
+			for (var i = nodeList.length - 1; i > -1; --i) {
+				if (nodeList[i].level > e.level) {
+					return nodeList[i];
+				}
+			}
+		}
 	}
 }
