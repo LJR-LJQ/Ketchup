2f4ef6ff37a484875dab209be260d45801eda9ba
zetsin <zetsin@gmail.com>
2013-05-07 14:40:44 +0800

    添加命令行支持
    格式：
    node [arg] main.js <input-file> [<output-file>]

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // [模块]
 var fs = require('fs'),
+	path = require('path'),
 	parser = require('./parser.js'),
 	outputer = require('./outputer.js');
 
@@ -10,9 +11,28 @@ var inputFileName,
 var inputFileContent,
 	outputFileContent;
 
+var arguments,
+	fsRealPath,
+	argRealPath;
+
 // [流程]
 inputFileName = 'input.txt';
 outputFileName = 'output.html';
+arguments = process.argv;
+fsRealPath = fs.realpathSync('.');
+for(var i = 1; i < arguments.length; i++) {
+	argRealPath = path.normalize(arguments[i]);
+	argRealPath = path.dirname(argRealPath);
+	if(argRealPath == fsRealPath) {
+		arguments = arguments.splice(i + 1);
+		break;
+	}
+}
+inputFileName = typeof arguments[0] == 'undefined' ? '' : arguments[0];
+outputFileName = typeof arguments[1] == 'undefined' ? inputFileName : arguments[1];
+if(outputFileName.indexOf('.html') < 0) outputFileName += '.html';
+console.log(inputFileName);
+console.log(outputFileName);
 
 if (!fs.existsSync(inputFileName)) {
 	console.log('文件不存在：' + inputFileName);
